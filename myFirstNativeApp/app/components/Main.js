fea3962e959f105db4e3e357d26770f8bd184b75
neal.bharucha@msichicago.org <nealbharucha@yahoo.com>
2018-04-25 14:01:23 -0500

    textInput, header, footer, styling

--- myFirstNativeApp/app/components/Main.js
+++ myFirstNativeApp/app/components/Main.js
@@ -6,20 +6,64 @@ import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from 
 */
 
 export default class Main extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Main Component</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.header}>
+					<Text>- Header -</Text>        
+				</View>
+				<ScrollView style={styles.scrollContainer}>
+					<Text style={styles.headerText}>Neal</Text>
+				</ScrollView>
+				<View style={styles.footer}>
+					<TextInput 
+						style={styles.textInput}
+						placeholder='note'
+						placeholderTextColor='crimson'
+						underlineColorAndroid='transparent'
+					>
+					</TextInput>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: 'white',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	header: {
+		backgroundColor: 'lightgray',
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderBottomWidth: 3,
+		borderBottomColor: 'crimson',
+	},
+	headerText: {
+		color: 'black',
+		fontSize: 18,
+	},
+	scrollContainer: {
+		flex: 1,
+		marginBottom: 100,
+	},
+	footer: {
+		position: 'absolute',
+		bottom: 0,
+		left: 0,
+		right: 0,
+		zIndex: 10,
+	},
+	textInput: {
+		alignSelf: 'stretch',
+		color: 'crimson',
+		padding: 20,
+		backgroundColor: 'lightgray',
+		borderTopWidth: 3,
+		borderTopColor: 'crimson',
+	}
 });
